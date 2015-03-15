62fbdcef62922f9c2a57dfc7ae4d160c0baaeff2
bedna-KU <kdesi@kdesi.sk>
2015-03-15 22:03:16 +0100

    auto font size

--- .icewm/contrib/volume_down.sh
+++ .icewm/contrib/volume_down.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-
+xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f1)
+font_width=`expr $xaxis / 40`
 pactl set-sink-volume 0 -- "-10%"
-echo `(pactl list sinks | grep "Volume: 0:")| awk '{print $3}'`| aosd_cat -n "Serif 40" -f 1 -u 300 -o 1 -p 8 -x -30
+echo `(pactl list sinks | grep "Volume: 0:")| awk '{print $3}'`| aosd_cat -n "Serif $font_width" -f 1 -u 300 -o 1 -p 8 -x -30
