0c3082a3bb4610c2799a13ed80b12a82ab824b36
bedna-KU <kdesi@kdesi.sk>
2015-03-15 19:10:29 +0100

    more changes

--- .icewm/contrib/system_stat.sh
+++ .icewm/contrib/system_stat.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 # Show system status on OSD
 
-free -h | grep -i - |  aosd_cat -n "Serif 40" -u 3000 -p 4
-free -h | grep -i swap |  aosd_cat -n "Serif 40" -u 3000 -p 4
-grep 'cpu ' /proc/stat | awk '{usage=int(($2+$4)*100/($2+$4+$5))} END {print "CPU " usage "%"}' |  aosd_cat -n "Serif 40" -u 4000 -p 4
+#free -h | grep -i - |  aosd_cat -n "Serif 40" -u 3000 -p 4
+#free -h | grep -i swap |  aosd_cat -n "Serif 40" -u 3000 -p 4
+#grep 'cpu ' /proc/stat | awk '{usage=int(($2+$4)*100/($2+$4+$5))} END {print "CPU " usage "%"}' |  aosd_cat -n "Serif 40" -u 4000 -p 4
+# aaa=`free -h` && echo $aaa |  aosd_cat -n "Serif 40" -u 3000 -p 4
+head_mem='                                  used       free'
+mem=`free -h | grep -i - | sed 's/-\/+ buffers\/cache:/Mem:                 /g'`
+swap=`free -h | grep -i swap`
+cpu=`grep 'cpu ' /proc/stat | awk '{usage=int(($2+$4)*100/($2+$4+$5))} END {print "CPU: " usage "%"}'`
+
+echo -e "$head_mem\r$mem\r$swap\r$cpu" |  aosd_cat -n "Serif 40" -u 3000 -p 4
