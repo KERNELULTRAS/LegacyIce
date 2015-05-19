73ca3c09444db18af4a38bd686295af43a3cba2d
bedna-KU <kdesi@kdesi.sk>
2015-05-19 15:37:11 +0200

    added volume control for PA and Alsa

--- .icewm/contrib/volume_up.sh
+++ .icewm/contrib/volume_up.sh
@@ -1,9 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f1)
 font_width=`expr $xaxis / 40`
-
-#pactl set-sink-volume 0 -- "+10%"
-#echo `(pactl list sinks | grep "Volume: 0:")| awk '{print $3}'`| aosd_cat -n "Serif $font_width" -f 1 -u 300 -o 1 -p 8 -x -30
-
-amixer -c 0 set Master 5%+
-echo `amixer get Master` | egrep -o "[0-9]+%" | aosd_cat -n "Serif $font_width" -f 1 -u 300 -o 1 -p 8 -x -30
+if [ -x "$(command -v pactl)" ];
+then
+	pactl set-sink-volume 0 -- "+10%"
+	echo `(pactl list sinks | grep "Volume: 0:")| awk '{print $3}'`| aosd_cat -n "Serif $font_width" -f 1 -u 300 -o 1 -p 8 -x -30
+else
+	amixer -c 0 set Master 5%+
+	echo `amixer get Master` | egrep -o "[0-9]+%" | aosd_cat -n "Serif $font_width" -f 1 -u 300 -o 1 -p 8 -x -30
+fi
