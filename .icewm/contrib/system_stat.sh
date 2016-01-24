8ab4c0e7812d3479a958d6083878e0f71cf34cff
bedna-KU <kdesi@kdesi.sk>
2016-01-24 10:52:11 +0100

    change volume and shutdown

--- .icewm/contrib/system_stat.sh
+++ .icewm/contrib/system_stat.sh
@@ -10,4 +10,4 @@ swap=`free -h | grep -i swap`
 cpu_cores=`grep -c ^processor /proc/cpuinfo`
 cpu=`top -bn 1 | awk 'NR>7{s+=$9} END {print "CPU: "s/'$cpu_cores}`'%'
 
-echo -e "$head_mem\n$mem\n$swap\n$cpu" |  osd_cat -A center -p middle -f -adobe-helvetica-bold-*-*-*-$font_width-120-*-*-*-*-*-* -cgreen -d3 -O3
+echo -e "$head_mem\n$mem\n$swap\n$cpu" |  osd_cat -A center -p middle -f -adobe-helvetica-bold-r-*-*-$font_width-120-*-*-*-*-*-* -cgreen -d3 -O3
