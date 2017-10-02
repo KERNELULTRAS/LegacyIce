b138ca11cabd4324c4fa33ba5d1a55a8501634bd
bedna-KU <kdesi@kdesi.sk>
2017-10-02 19:17:12 +0200

    change volumecontrol for PA

--- .icewm/contrib/volume_down.sh
+++ .icewm/contrib/volume_down.sh
@@ -6,7 +6,7 @@ y_offset=`expr $xaxis / 40`
 FONT="-adobe-helvetica-bold-*-*-*-60-*-*-*-*-*-*-*"
 if [ -x "$(command -v pactl)" ];
 then
-	pactl set-sink-volume 0 -- "-5%"
+	pactl set-sink-volume 0 "-5%"
 	echo `(pactl list sinks | grep "Volume: front")| awk '{print $9}'`| osd_cat -A right -p bottom -f -adobe-helvetica-bold-r*-*-*-$font_width-120-*-*-*-*-*-*  -cgreen -d1 -i30 -o-100 -O3
 else
 	amixer -c 0 set Master 5%-
