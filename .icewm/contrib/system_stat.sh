f49fe4f52a1624d4ec33ba0ae19ddd5c1c60bcbb
bedna-KU <kdesi@kdesi.sk>
2018-11-18 12:59:07 +0100

    change system_stat and volume

--- .icewm/contrib/system_stat.sh
+++ .icewm/contrib/system_stat.sh
@@ -3,11 +3,13 @@
 
 xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f1)
 font_width=`expr $xaxis / 40`
-head_mem='                                     used       free'
-# mem=`free -h | grep -i - | sed 's/-\/+ buffers\/cache:/Mem:/g'`
-mem=`free -h | grep -i buffers/cache`
-swap=`free -h | grep -i swap`
+mem_total=`free -h | awk '{print $2}' | sed -n 2p`
+mem_free=`free -h | awk '{print $7}' | sed -n 2p`
+mem="Mem total: $mem_total - Mem free: $mem_free"
+swap_total=`free -h | awk '{print $2}' | sed -n 3p`
+swap_free=`free -h | awk '{print $4}' | sed -n 3p`
+swap="Swap total: $swap_total - Swap free: $swap_free"
 cpu_cores=`grep -c ^processor /proc/cpuinfo`
 cpu=`top -bn 1 | awk 'NR>7{s+=$9} END {print "CPU: "s/'$cpu_cores}`'%'
 
-echo -e "$head_mem\n$mem\n$swap\n$cpu" |  osd_cat -A center -p middle -f -adobe-helvetica-bold-r-*-*-$font_width-120-*-*-*-*-*-* -cgreen -d3 -O3
+echo -e "$mem\n$swap\n$cpu" |  osd_cat -A center -p middle -f -adobe-helvetica-bold-r-*-*-$font_width-120-*-*-*-*-*-* -cgreen -d3 -O3
