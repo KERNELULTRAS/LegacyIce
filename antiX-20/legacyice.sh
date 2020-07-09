285686ff2b158f451556622ea73f3097ded00464
Mario <kdesi@kdesi.sk>
2020-07-09 09:55:07 +0200

    Fixed Core tools

--- antiX-20/legacyice.sh
+++ antiX-20/legacyice.sh
@@ -137,7 +137,7 @@ echo -e "\e[93m- Install programs\e[38;5;46m\n"
 ### Core tools
 sudo apt-get --yes install autopoint autoconf automake autogen bc curl debfoster earlyoom git g++ \
 hsetroot intltool imagemagick key-mon libgdk-pixbuf2.0-dev libgnome-menu-3-dev libgtk2.0-dev libice-dev libsm-dev \
-libtool libwnck-dev libx11-dev libxext-dev linuxdoc-tools lximage-qt\
+libtool libwnck-dev libx11-dev libxext-dev linuxdoc-tools lximage-qt \
 m4 numlockx pavumeter pavucontrol paprefs pcmanfm-qt qterminal \
 subversion synaptic telnet wget whois wmctrl software-properties-common xfce4-notifyd xosd-bin
 
